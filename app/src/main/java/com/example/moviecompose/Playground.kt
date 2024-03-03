8252741e4a7e66e4a2032741d0abe215d0ad7747
Anelkad <anel.k@1fit.app>
2024-03-03 22:03:52 +0500

    enable scroll

--- app/src/main/java/com/example/moviecompose/Playground.kt
+++ app/src/main/java/com/example/moviecompose/Playground.kt
@@ -1,17 +1,25 @@
 package com.example.moviecompose
 
+import android.util.Log
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.animateOffsetAsState
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.gestures.rememberTransformableState
 import androidx.compose.foundation.gestures.transformable
+import androidx.compose.foundation.interaction.collectIsDraggedAsState
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -19,22 +27,28 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
+import java.util.Random
+import kotlin.math.abs
 
 @Composable
 fun TransformableImage(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    scale: Float = 1f,
+    updateScale: (Float) -> Unit
 ) {
     BoxWithConstraints {
         var offset by remember { mutableStateOf(Offset.Zero) }
-        var scale by remember { mutableStateOf(1f) }
+        var scale by remember { mutableFloatStateOf(scale) }
         val transformableState =
             rememberTransformableState { zoomChange, panChange, _ ->
                 scale *= zoomChange
+                updateScale(scale)
 
-                val extraWidth = (scale - 1) * constraints.maxWidth
-                val extraHeight = (scale - 1) * constraints.maxHeight
+                val extraWidth = abs(scale - 1) * constraints.maxWidth
+                val extraHeight = abs(scale - 1) * constraints.maxHeight
 
                 val maxX = extraWidth / 2
                 val maxY = extraHeight / 2
@@ -52,14 +66,31 @@ fun TransformableImage(
             targetValue = offset
         )
         val animScale by animateFloatAsState(
-            targetValue = if (!transformableState.isTransformInProgress && scale > 5F) {
-                offset = Offset.Zero
-                scale = 5F
-                5F
+            targetValue = if (transformableState.isTransformInProgress) {
+                if (scale >= 1F) scale else {
+                    scale = 1f
+                    updateScale(1f)
+                    1F
+                }
             } else {
-                scale
+                when {
+                    scale < 1F -> {
+                        offset = Offset.Zero
+                        scale = 1F
+                        updateScale(1f)
+                        1f
+                    }
+                    scale > 5F -> {
+                        updateScale(5f)
+                        scale = 5F
+                        5F
+                    }
+                    else -> scale
+                }
             }
         )
+        val random = remember { Random() }
+        val color = remember { Color(random.nextInt(256), random.nextInt(256), random.nextInt(256)) }
         Image(
             modifier = modifier
                 .graphicsLayer(
@@ -69,8 +100,22 @@ fun TransformableImage(
                     translationX = offset.x,
                     translationY = offset.y,
                 )
-                .transformable(transformableState)
-                .background(Color.Blue)
+                .transformable(
+                    transformableState,
+                    enabled = scale != 1f
+                )
+                .pointerInput(Unit) {
+                    detectTapGestures(
+                        onDoubleTap = { tapOffset ->
+                            scale *= 3
+                            if (scale > 5) {
+                                scale = 1f
+                                offset = Offset.Zero
+                            }
+                        }
+                    )
+                }
+                .background(color)
                 .fillMaxSize(),
             imageVector = Icons.Default.DateRange,
             contentDescription = "studio image",
@@ -79,8 +124,46 @@ fun TransformableImage(
     }
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Preview(showBackground = true)
 @Composable
 fun TransformableImagePreview() {
-    TransformableImage()
+    val pagerState = rememberPagerState { 5 }
+    var scale by remember {
+        mutableFloatStateOf(1f)
+    }
+    Log.d("qwerty enable", scale.toString())
+    val enableScroll by remember {
+        derivedStateOf { scale == 1f }
+    }
+    HorizontalPager(
+        state = pagerState,
+        userScrollEnabled = enableScroll
+    ) {
+        page ->
+        Log.d("qwerty", enableScroll.toString())
+        TransformableImage(
+            modifier = Modifier
+                .fillMaxSize()
+                .pointerInput(enableScroll) {
+                    val screenWidth = size.width
+                    detectTapGestures(
+                        onPress = { offset ->
+                            if (offset.x < screenWidth / 6) {
+                                if (page > 0) {
+                                    pagerState.scrollToPage(pagerState.currentPage - 1)
+                                }
+                            } else {
+                                if (offset.x > screenWidth * 5 / 6) {
+                                    if (page < pagerState.pageCount) {
+                                        pagerState.scrollToPage(pagerState.currentPage + 1)
+                                    }
+                                }
+                            }
+                        }
+                    )
+                },
+            updateScale = { scale = it }
+        )
+    }
 }
